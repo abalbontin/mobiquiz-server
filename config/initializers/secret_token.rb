ed818bef91b311019b7b6d8d8b83afca4f800b1d
abalbontin <alvaro@mobivery.com>
2013-09-23 19:46:55 +0200

    Creación del proyecto nuevo.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MobiquizServer::Application.config.secret_key_base = '510dda6cab1edf142353832435c50dc8a4ca450c3fa4bd292337914d4f4449857e4eb9a4b913aa1682ae1a7f8dd6fc52ba4facf0cd862775943e84c13a708a9c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MobiquizServer::Application.config.session_store :cookie_store, key: '_mobiquiz-server_session'
