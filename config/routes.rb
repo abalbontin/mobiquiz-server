bb8aceca73b8cb6dba9ae0e69eeb53cd645123f2
abalbontin <alvaro@mmip.es>
2013-09-24 12:21:51 +0200

    Ahora se muestra un formulario para las nuevas preguntas.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 MobiquizServer::Application.routes.draw do
+
+  resources :questions
+
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
