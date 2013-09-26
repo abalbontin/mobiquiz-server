1bcf3f15b39b288c1fb296c99941bfa1b1d699e7
abalbontin <alvaro@mmip.es>
2013-09-26 13:02:23 +0200

    Ahora los campos Solución y Dificultad se rellenan mediante un selector.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,5 @@
 class Question < ActiveRecord::Base
-  validates_presence_of :question, :answer1, :answer2, :answer3, :answer4, :solution, :difficulty
-  validates_numericality_of :solution, :difficulty
-  validates_inclusion_of :solution, :in => 1..4, :message => "debe contener una de las opciones disponibles: 1, 2, 3 ó 4"
-  validates_inclusion_of :difficulty, :in => 1..3, :message => "debe contener una de las opciones disponibles: 1, 2 ó 3"
+  validates_presence_of :question, :answer1, :answer2, :answer3, :answer4
+  validates_inclusion_of :solution, :in => 1..4, :message => "debe indicar la respuesta correcta"
+  validates_inclusion_of :difficulty, :in => 1..3, :message => "debe indicar un nivel de dificultad: Fácil, Media o Difícil"
 end
